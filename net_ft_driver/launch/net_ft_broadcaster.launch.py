a6d3c8d15d3e6494d972fae149cd0938a20c82e8
Matthew Powelson <powelson.matthew@gmail.com>
2025-08-20 16:15:16 -0400

    test

--- net_ft_driver/launch/net_ft_broadcaster.launch.py
+++ net_ft_driver/launch/net_ft_broadcaster.launch.py
@@ -83,7 +83,7 @@ def launch_setup(context, *args, **kwargs):
     control_node = launch_ros.actions.Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description_param, ft_controller],
+        parameters=[ft_controller],
     )
 
     robot_state_publisher_node = launch_ros.actions.Node(
